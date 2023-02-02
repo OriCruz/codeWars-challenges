74bd54fb678401911a52bbb346407b4d756d496d
OriCruz <oc080901@gmail.com>
2023-02-02 10:24:53 -0500

    dedug sum of integers kata

---
+++ 7-kyu/debugSumOfInts.js
@@ -0,0 +1,10 @@
+function getSumOfDigits(integer) {
+    let sum = 0;
+    var digits =  Math.floor(integer).toString();
+    for(var i = 0; i < digits.length; i ++) {
+        sum+=parseInt(digits[i]);
+    }
+    return sum;
+  }
+
+  console.log(getSumOfDigits(123)); 
